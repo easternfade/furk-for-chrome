d9dd6e14e00bf349fcc6c48f000c2f00e271b824
easternfade <easternfade@users.noreply.github.com>
2017-01-14 18:31:35 +1100

    Convert options page to requirejs

--- src/scripts/app/furkForChromeOptions.js
+++ src/scripts/app/furkForChromeOptions.js
@@ -1,20 +1,21 @@
 ﻿//'use strict';
 
-var FurkForChromeOptions = (function () {
+define(["app/furkForChromeStorage", "app/furkAPI"], function (storage, furkAPI) {
+//define(['require', 'app/furkForChromeStorage'], function(require) {
 
-    this.extractApiKey = function(textBoxTarget) {
+    this.extractApiKey = function (textBoxTarget) {
         var xhr = new XMLHttpRequest();
-        xhr.open("GET", FurkAPI.FurkApiPage(), true);
-        xhr.onreadystatechange = function() {
-          if (xhr.readyState == 4) {
-            if (xhr.responseText !== "") {
-                var re = /<li>api_key.+?Your API key is:.*?([0-9a-f]+).*?<\/li>/gm;
-                var matches = re.exec(xhr.responseText);
-                if (matches) {
-                    textBoxTarget.innerText = matches[1];
+        xhr.open("GET", furkAPI.FurkApiPage(), true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4) {
+                if (xhr.responseText !== "") {
+                    var re = /<li>api_key.+?Your API key is:.*?([0-9a-f]+).*?<\/li>/gm;
+                    var matches = re.exec(xhr.responseText);
+                    if (matches) {
+                        textBoxTarget.innerText = matches[1];
+                    }
                 }
             }
-          }
         };
         xhr.send();
     };
@@ -24,7 +25,7 @@ var FurkForChromeOptions = (function () {
         // Restores select box state to saved value from localStorage.
         restoreOptions: function () {
             //chrome.storage.sync.get(FurkForChromeStorage.storageKeys.API_KEY, function(items) {
-            FurkForChromeStorage.Get('API_KEY', function(result, item) {
+            storage.Get('API_KEY', function (result, item) {
                 if (result.status === 'OK') {
                     var textBox = document.getElementById("furkForChrome_apiKey");
                     if (textBox) { // && items[FurkForChromeStorage.storageKeys.API_KEY]) {
@@ -33,7 +34,7 @@ var FurkForChromeOptions = (function () {
                 }
             });
 
-            FurkForChromeStorage.Get('TTS_ENABLE', function(result, item) {
+            storage.Get('TTS_ENABLE', function (result, item) {
                 if (result.status === 'OK') {
                     var checkBox = document.getElementById("furkForChrome_ttsEnable");
                     if (checkBox) {
@@ -46,17 +47,26 @@ var FurkForChromeOptions = (function () {
         saveOptions: function () {
             var apiKey = document.getElementById("furkForChrome_apiKey").value;
             if (apiKey) {
-                var keyName = FurkForChromeStorage.storageKeys.API_KEY;
-                chrome.storage.sync.set({ keyName: apiKey }, function() {
-                    
+                var keyName = storage.storageKeys.API_KEY;
+                chrome.storage.sync.set({ keyName: apiKey }, function () {
+
                 });
             }
 
             var ttsEnable = document.getElementById("furkForChrome_ttsEnable").value;
             if (ttsEnable) {
-                var keyName = FurkForChromeStorage.storageKeys.TTS_ENABLE;
-                chrome.storage.sync.set({ keyName: ttsEnable }, function() {});
+                var keyName = storage.storageKeys.TTS_ENABLE;
+                chrome.storage.sync.set({ keyName: ttsEnable }, function () { });
             }
+        },
+        loadExtensionInfo: function () {
+            chrome.management.getSelf(function (extensionInfo) {
+                var textBox = document.getElementById('ext_name');
+
+                if (textBox) {
+                    textBox.innerText = extensionInfo.name + ' ' + extensionInfo.version;
+                }
+            });
         },
         // Attempts to scrape the API key from Furk
         findApiKey: function () {
@@ -66,20 +76,23 @@ var FurkForChromeOptions = (function () {
             if (textBox) {
                 extractApiKey(textBox);
             }
-            
+
         },
         init: function () {
+            // Populate dynamic content
+            this.loadExtensionInfo();
+
             // Load options from local storage
-            FurkForChromeOptions.restoreOptions();
+            this.restoreOptions();
 
             // Bind options page event handlers
 
             // 1. Save  then close tab on button click
             var closeButton = document.getElementById("btn_close");
             if (closeButton) {
-                closeButton.onclick = function() { 
-                    chrome.tabs.getCurrent(function(tab) {
-                        FurkForChromeOptions.saveOptions();
+                closeButton.onclick = function () {
+                    chrome.tabs.getCurrent(function (tab) {
+                        this.saveOptions();
                         chrome.tabs.remove(tab.id);
                     });
                     return false;
@@ -90,18 +103,22 @@ var FurkForChromeOptions = (function () {
             // 2. Autoload API key
             var findApiLink = document.getElementById("a_findApiKey");
             if (findApiLink) {
-                findApiLink.onclick = function() { alert('Sorry, work in progress!'); }; // FurkForChromeOptions.findApiKey;
+                findApiLink.onclick = function () { alert('Sorry, work in progress!'); }; // FurkForChromeOptions.findApiKey;
             }
 
             // 3. Manual save button
             var saveButton = document.getElementById("btn_save");
             if (saveButton) {
-                saveButton.onclick = FurkForChromeOptions.saveOptions;
-            }            
+                saveButton.onclick = this.saveOptions;
+            }
         }
     };
-}());
-
-window.addEventListener('DOMContentLoaded', function () {
-    FurkForChromeOptions.init();
 });
+
+// var FurkForChromeOptions = (function () {
+
+// }());
+
+// window.addEventListener('DOMContentLoaded', function () {
+//     FurkForChromeOptions.init();
+// });
