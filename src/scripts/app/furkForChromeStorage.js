6282da3f4fab7a0f7979f9aab8053b74cd28d7d1
easternfade <easternfade@users.noreply.github.com>
2017-01-14 21:57:08 +1100

    Cleaning up options page - using skeleton css

--- src/scripts/app/furkForChromeStorage.js
+++ src/scripts/app/furkForChromeStorage.js
@@ -4,27 +4,28 @@
 // var FurkForChromeStorage = (function() {
 define(function () {
 
-   return {
-      storageKeys: {
-         'API_KEY': 'furkForChrome_apiKey',
-         'TTS_ENABLE': 'furkForChrome_ttsEnable'
-      },
-      Get: function(storageKey, itemCallback) {
+    var module = {
+        storageKeys: {
+            'API_KEY': 'furkForChrome_apiKey',
+            'TTS_ENABLE': 'furkForChrome_ttsEnable'
+        },
+        Get: function (storageKey, itemCallback) {
 
-         if ( ! this.storageKeys[storageKey] ) {
-            console.log("Storage key " + storageKey + " is not a valid key name.");
-            itemCallback( {status: 'Error' }, undefined);
-            return;
-         }
-
-         chrome.storage.sync.get(this.storageKeys[storageKey], function(items) {
-            if (items[this.storageKeys[storageKey]]) {
-               itemCallback( {status: 'OK' }, items[this.storageKeys[storageKey]]);
-            } else {
-               itemCallback( {status: 'Error' }, undefined);
+            if (!module.storageKeys[storageKey]) {
+                console.log("Storage key " + storageKey + " is not a valid key name.");
+                itemCallback({ status: 'Error' }, undefined);
+                return;
             }
-         });
-      }
-   };
+
+            chrome.storage.sync.get(module.storageKeys[storageKey], function (items) {
+                if (items[module.storageKeys[storageKey]]) {
+                    itemCallback({ status: 'OK' }, items[module.storageKeys[storageKey]]);
+                } else {
+                    itemCallback({ status: 'Error' }, undefined);
+                }
+            });
+        }
+    };
+    return module;
 });
 // }());
